b8eff5d14aa0ac704e7b9af29a458d3f47ac1adc
stressedball <tserchesi@gmail.com>
2023-01-08 16:58:55 +0100

    Updates

---
+++ precache-manifest.59e80630c68629589b92cf4fb5278714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d18f15999a60abe9356",
+    "url": "/cv-app/static/css/main.e9ab33fb.chunk.css"
+  },
+  {
+    "revision": "1d18f15999a60abe9356",
+    "url": "/cv-app/static/js/main.1d18f159.chunk.js"
+  },
+  {
+    "revision": "e3e49fd16a418e171f4d",
+    "url": "/cv-app/static/js/1.e3e49fd1.chunk.js"
+  },
+  {
+    "revision": "5257d31460df784bef29",
+    "url": "/cv-app/static/js/runtime~main.5257d314.js"
+  },
+  {
+    "revision": "9bb2c150aa78938f30db69ab360ea947",
+    "url": "/cv-app/static/media/delete-svgrepo-com.9bb2c150.svg"
+  },
+  {
+    "revision": "69337bee96e148526e400b2a411e69ba",
+    "url": "/cv-app/static/media/delete-trash-remove-svgrepo-com.69337bee.svg"
+  },
+  {
+    "revision": "eb05118a7baedbbacd286afe51d070e1",
+    "url": "/cv-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d27f2338bf91a121044680d364fbf38d.js"
+  "/cv-app/precache-manifest.59e80630c68629589b92cf4fb5278714.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/cv-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

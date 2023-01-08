c6b024b58f5c72d00cf90754f192921eefe7b98a
stressedball <tserchesi@gmail.com>
2023-01-08 16:51:26 +0100

    push for gh

---
+++ precache-manifest.d27f2338bf91a121044680d364fbf38d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d18f15999a60abe9356",
+    "url": "/static/css/main.e9ab33fb.chunk.css"
+  },
+  {
+    "revision": "1d18f15999a60abe9356",
+    "url": "/static/js/main.1d18f159.chunk.js"
+  },
+  {
+    "revision": "e3e49fd16a418e171f4d",
+    "url": "/static/js/1.e3e49fd1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9bb2c150aa78938f30db69ab360ea947",
+    "url": "/static/media/delete-svgrepo-com.9bb2c150.svg"
+  },
+  {
+    "revision": "69337bee96e148526e400b2a411e69ba",
+    "url": "/static/media/delete-trash-remove-svgrepo-com.69337bee.svg"
+  },
+  {
+    "revision": "e5f4db7d6ee9d656484ec7c381d04682",
+    "url": "/index.html"
+  }
+];
